769beb294217b86034d7a4d5efb5873ef65b5699
kaifkhan <kkaif2687@gmail.com>
2024-08-08 19:54:10 +0530

    change

---
+++ src/selection/selection_sort.java
@@ -0,0 +1,43 @@
+package selection;
+
+import java.util.Scanner;
+
+
+public class selection_sort {
+    int[] sort(int[] arr,int n){
+        for(int i=0; i<n-1 ;i++){
+            int min=i;
+            for (int j=i;j<=n-1;j++) {
+                if (arr[j] <arr[min]) {
+                    min = j;
+                }
+
+            }
+            int tamp=arr[i];
+            arr[i]=arr[min];
+            arr[min]=tamp;
+        }
+        return arr;
+    }
+
+}
+class result{
+    public static void main(String[] args) {
+        System.out.println("Enter the size of array");
+        Scanner input=new Scanner(System.in);
+        int n=input.nextInt();
+        int[] arr=new int[n];
+        System.out.println("Enter all value in array");
+        for (int i=0;i<n;i++){
+            arr[i]=input.nextInt();
+        }
+        selection_sort obj=new selection_sort();
+        arr=obj.sort(arr,n);
+        for (int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+            }
+
+
+    }
+
+}
